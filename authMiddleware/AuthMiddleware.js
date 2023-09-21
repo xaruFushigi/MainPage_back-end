0e957d6aea3bc927b24fadd206cdfd3de5fcc7f3
xaruFushigi <xaru.fushigi@gmail.com>
2023-09-21 20:27:40 +0900

    Update AuthMiddleware.js

--- authMiddleware/AuthMiddleware.js
+++ authMiddleware/AuthMiddleware.js
@@ -5,18 +5,22 @@ const AuthMiddleware = (req, res, next) => {
   const accessToken = req.header("accessToken");
 
   if (!accessToken) {
-    return res.status(405).json({ error: "Log in to make comments" });
+    return res
+      .status(405)
+      .json({ error: "Log In to be able to access portfolio" });
   } else {
     try {
       if (accessToken !== "null") {
         const validToken = verify(accessToken, process.env.SESSION_SECRET);
-        const expirationTime = Math.floor(Date.now() / 1000) + 30; // Token expires in 30 seconds
+        const expirationTime = Math.floor(Date.now() / 1000); // Token expires in 30 seconds
         if (validToken.exp > expirationTime) {
           req.user = validToken;
           return next();
         }
       } else {
-        return res.status(405).json({ error: "Log in to make comments" });
+        return res
+          .status(405)
+          .json({ error: "Log In to be able to access portfolio" });
       }
     } catch (error) {
       return res.status(400).json({ error: error });
