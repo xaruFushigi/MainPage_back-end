6705fa781ae53e5b15976c82799e5149051ce3a6
xaruFushigi <xaru.fushigi@gmail.com>
2023-09-22 22:13:08 +0900

    Update UserDatabase.js

--- routes/User/UserDatabase.js
+++ routes/User/UserDatabase.js
@@ -8,6 +8,7 @@ const CreateUserInDatabase = async ({
   bcrypt,
 }) => {
   // hashing user's password and creating new user
+  console.log(username, password, firstname, lastname, isAdmin, Users, bcrypt);
   bcrypt.hash(password, 10).then((hash) => {
     Users.create({
       username: username,
