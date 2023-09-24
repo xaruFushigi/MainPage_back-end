064b6f216907877bdaf7d73ae139316f008191a9
xaruFushigi <xaru.fushigi@gmail.com>
2023-09-24 14:11:20 +0900

    Update UserDatabase.js

--- routes/User/UserDatabase.js
+++ routes/User/UserDatabase.js
@@ -34,23 +34,22 @@ const logUserIn = async ({ username, password, Users, bcrypt, sign }) => {
     // if username does not exist in the database
     if (!user) {
       return false;
-    } else {
-      // if password does not mathc/exits in the database
-      bcrypt.compare(password, user.password).then((match) => {
-        if (!match) {
-          return false;
-        }
-        // Set the expiration time for the token (e.g., 1 day)
-        const expiresIn = "1h";
-        // jsonwebtoken
-        const accessToken = sign(
-          { username: user.username, id: user.id }, // user input information
-          process.env.SESSION_SECRET, // session token secret
-          { expiresIn }, // Options object including expiresIn
-        );
-        return { accessToken, id: user.id, username: user.username };
-      });
     }
+    // if password does not mathc/exits in the database
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        return false;
+      }
+      // Set the expiration time for the token (e.g., 1 day)
+      const expiresIn = "1h";
+      // jsonwebtoken
+      const accessToken = sign(
+        { username: user.username, id: user.id }, // user input information
+        process.env.SESSION_SECRET, // session token secret
+        { expiresIn }, // Options object including expiresIn
+      );
+      return { accessToken, id: user.id, username: user.username };
+    });
   } catch (error) {
     throw error;
   }
